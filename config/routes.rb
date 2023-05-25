4d8632fd9760f022ba0f693648297c933c3ff847
Lucas_Souza <souza.vilela.lucas@gmail.com>
2023-05-25 18:05:16 +0200

    first day

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,15 @@
 Rails.application.routes.draw do
+  root to: "lists#index"
+  # resources :bookmarks
+  resources :lists, only: [:index, :show, :new, :create] 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
 end
+
+# As a user, I can see all my movie lists
+# As a user, I can create a movie list
+# As a user, I can see the details of a movie list
+# As a user, I can bookmark a movie inside a movie list
+# As a user, I can destroy a bookmark
--- db/seeds.rb
+++ db/seeds.rb
@@ -4,4 +4,26 @@
 # Examples:
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+#   Character.create(name: "Luke", movie: movies.first
+
+require 'open-uri'
+require 'json'
+
+puts 'Cleaning database...'
+Movie.destroy_all
+puts 'Database cleaned!'
+
+url = 'https://tmdb.lewagon.com/movie/top_rated'
+
+response = URI.open(url).read
+movies = JSON.parse(response)['results'] 
+movies.each do |movie|
+    puts "Creating #{movie['title']}..."
+    poster_url_base = "https://image.tmdb.org/t/p/original"
+    Movie.create(
+        title: movie['title'], 
+        overview: movie['overview'], 
+        poster_url: "#{poster_url_base}#{movie['poster_path']}",
+        rating: movie['vote_average']
+    )
+end
